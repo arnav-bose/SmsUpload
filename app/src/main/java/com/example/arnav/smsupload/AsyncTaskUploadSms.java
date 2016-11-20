5b187c951baab2b5795d9c3d4c2b5c1d99d3c9a7
Arnav <arnav-bose>
2016-11-20 19:48:16 +0530

    Changed File Upload

--- app/src/main/java/com/example/arnav/smsupload/AsyncTaskUploadSms.java
+++ app/src/main/java/com/example/arnav/smsupload/AsyncTaskUploadSms.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
-import android.os.Environment;
 import android.widget.Toast;
 
+import com.android.internal.http.multipart.MultipartEntity;
 import com.sromku.simple.storage.SimpleStorage;
 import com.sromku.simple.storage.Storage;
+
+import net.gotev.uploadservice.MultipartUploadRequest;
+import net.gotev.uploadservice.UploadNotificationConfig;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -16,17 +19,8 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.InputStreamEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 
 /**
@@ -36,13 +30,11 @@ public class AsyncTaskUploadSms extends AsyncTask<Void, Void, Void> {
 
     private Context contextUploadSms;
     private Activity activityUploadSms;
-    private ArrayList<String> arrayListSms;
     private ProgressDialog progressDialogUploadSms;
 
     public AsyncTaskUploadSms(Context context) {
         this.contextUploadSms = context;
         this.activityUploadSms = (Activity) context;
-        this.arrayListSms = arrayListSms;
     }
 
     @Override
@@ -71,17 +63,15 @@ public class AsyncTaskUploadSms extends AsyncTask<Void, Void, Void> {
         }
 
         File file = storage.getFile("/sms_upload/sms.txt");
+        String absolutePath = file.getAbsolutePath();
+        long size = file.getTotalSpace();
         try {
-            HttpClient httpclient = new DefaultHttpClient();
-
-            HttpPost httppost = new HttpPost(url);
-
-            InputStreamEntity reqEntity = new InputStreamEntity(new FileInputStream(file), -1);
-            reqEntity.setContentType("binary/octet-stream");
-            reqEntity.setChunked(true); // Send in multiple parts if needed
-            httppost.setEntity(reqEntity);
-            HttpResponse response = httpclient.execute(httppost);
-            //Do something with response...
+            String uploadId =
+                    new MultipartUploadRequest(contextUploadSms, "http://192.168.137.1/upload_sms/upload_all_sms.php")
+                            .addFileToUpload(absolutePath, "sms")
+                            .setNotificationConfig(new UploadNotificationConfig())
+                            .setMaxRetries(2)
+                            .startUpload();
 
         } catch (Exception e) {
             e.printStackTrace();
